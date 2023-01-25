79d0a872b34d268efdd30408423be5f8aa5db30c
Gabriel Moura <53822579+gabriel-moura2@users.noreply.github.com>
2023-01-25 19:18:55 +0000

    Preparando para adicionar livros

--- site-frontend/authentication.php
+++ site-frontend/authentication.php
@@ -7,10 +7,11 @@
 	$result = mysqli_query($db, $query) or die(mysqli_error($db));
 	
 	while($row = mysqli_fetch_array($result)) {
-
 		$id = $row['Id'];
-            
+		$password = $row['Senha'];
   	}
+	if ($_POST['password'] != $password) die("senha incorreta");
+		
 	$_SESSION["user"] = $id;
 ?>
 	<script type="text/javascript">
--- site-frontend/index.php
+++ site-frontend/index.php
@@ -8,7 +8,39 @@
 		if (isset($_SESSION["user"])) {
 			?>
 			<details open>
-				<summary>Pessoas</summary>
+				<summary>Livros</summary>
+				<a href="add_livro.php">Adicionar um livro</a>
+				<table>
+					<thead>
+						<tr>
+							<th>Capa</th>
+							<th>Titulo</th>
+							<th>Opções</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php                  
+							$query = 'SELECT * FROM Livro WHERE Id_Usuario='.$_SESSION['user'];
+							$result = mysqli_query($db, $query) or die (mysqli_error($db));
+
+							while ($row = mysqli_fetch_assoc($result)) {
+				                                
+								echo '<tr>';
+									echo '<td>'.$row['Imagem'].'</td>';
+									echo '<td>'. $row['Titulo'].'</td>';
+									echo '<td>';
+										echo '<a href="detail_livro.php?id='.$row['people_id'].'" >Ver livro</a><br/>';
+										echo '<a href="edit_livro.php?id='.$row['Id'].'">Editar</a><br/>';
+										echo '<a href="remove_livro.php?id='.$row['Id'].'">Remover</a>';
+									echo '</td>';
+								echo '</tr> ';
+							}
+						?>
+					</tbody>
+				</table>
+			</details>
+			<details>
+				<summary>Usuarios</summary>
 				<a href="add_pessoa.php">Adicionar uma pessoa</a>
 				<table>
 					<thead>
