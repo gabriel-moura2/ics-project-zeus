3990528a8cd9ba0ebd4d3646e65cc7f351369e46
Gabriel Moura <53822579+gabriel-moura2@users.noreply.github.com>
2023-01-26 17:48:00 +0000

    Configurando sql

--- site-frontend/edit_livro.php
+++ site-frontend/edit_livro.php
@@ -13,8 +13,22 @@
   $query = 'SELECT * FROM Livro WHERE Id='.$_GET['id'];
   $result = mysqli_query($db, $query) or die(mysqli_error($db));
 
-  while($row = mysqli_fetch)
+  while($row = mysqli_fetch_array($result)){
+    $id = $row['Id'];
+    $Title = $row['Titulo'];
+    $Link =  $row['Imagem'];
+    $Desc = $row['Descricao'];  
+  }
 
   ?>
+
+  <h2>Editando Livro</h2>
+  <br>
+  <form method="post" action="edit_livro_post.php">
+      <input type="hidden" name="id" value="<?php echo $id; ?>" />
+    <img width=200 style="float:left;margin:1em;" src=<?php echo $Link?>>
+    <input placeholder="Titulo" name="Titulo" value="<?php echo $Title?>"/> 
+    
+  </form>
   
 </body>
