1c637d4d4af9bb3d52d3a174d54b114d9f12bbf5
Gabriel Moura <53822579+gabriel-moura2@users.noreply.github.com>
2023-01-26 17:23:50 +0000

    Mudando estrutura do sql e adicionando remover livro

---
+++ site-frontend/remove_livro.php
@@ -0,0 +1,8 @@
+<?php
+  include('connection.php');
+  $query = 'DELETE FROM Livro Where Id = '.$_GET['id'];
+  $result = mysqli_query($db, $query) or die(mysqli_error($db));
+?>
+<script type="text/javascript">
+		window.location = "index.php";
+	</script>
